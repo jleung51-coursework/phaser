da41bc144f524eb41eb7e2fd93a42f383c0ab53b
Yi-Hsuan Wu <yihsuanw@sfu.ca>
2016-04-08 17:31:06 -0700

    add very basic and incomplete skeleton of PushServer.cpp

---
+++ src/PushServer.cpp
@@ -0,0 +1,77 @@
+/*
+  Push Server code for CMPT 276 Group Assignment, Spring 2016.
+
+  This server pushes status updates to all a user’s friends in the network.
+  
+  This server supports a single operation: push a status update to all friends
+  of this user.
+
+  This server handles disallowed method malformed request.
+*/
+
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <string>
+
+#include <cpprest/base_uri.h>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+#include <pplx/pplxtasks.h>
+
+#include <was/common.h>
+#include <was/storage_account.h>
+#include <was/table.h>
+
+#include "../include/TableCache.h"
+
+#include "../include/make_unique.h"
+
+#include "../include/azure_keys.h"
+
+#include "../include/ServerUtils.h"
+
+using azure::storage::cloud_storage_account;
+using azure::storage::storage_credentials;
+using azure::storage::storage_exception;
+using azure::storage::cloud_table;
+using azure::storage::cloud_table_client;
+using azure::storage::edm_type;
+using azure::storage::entity_property;
+using azure::storage::table_entity;
+using azure::storage::table_operation;
+using azure::storage::table_query;
+using azure::storage::table_query_iterator;
+using azure::storage::table_result;
+
+using pplx::extensibility::critical_section_t;
+using pplx::extensibility::scoped_critical_section_t;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::make_pair;
+using std::pair;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+using web::http::http_headers;
+using web::http::http_request;
+using web::http::methods;
+using web::http::status_codes;
+using web::http::uri;
+
+using web::json::value;
+
+using web::http::experimental::listener::http_listener;
+
+using prop_vals_t = vector<pair<string,value>>;
+
+constexpr const char* def_url = "http://localhost:34574";
