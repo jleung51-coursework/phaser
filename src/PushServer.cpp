3141ba21465be532ca814cda8907e67bd78b189a
Daphne Chong <dca74@sfu.ca>
2016-04-10 23:16:07 -0700

    add code to implement PushServer

--- src/PushServer.cpp
+++ src/PushServer.cpp
@@ -32,7 +32,7 @@
 
 #include "../include/make_unique.h"
 
-#include "../include/azure_keys.h"
+//#include "../include/azure_keys.h"
 
 #include "../include/ServerUtils.h"
 
@@ -76,17 +76,178 @@ using prop_vals_t = vector<pair<string,value>>;
 
 constexpr const char* def_url = "http://localhost:34574";
 
-TableCache table_cache {};
+const string push_status_op {"PushStatus"};
+
+//TableCache table_cache {};
+
+//---------------------------------------------------------------------------------------
+// useful functions from authserver
+
+/*
+  Convert properties represented in Azure Storage type
+  to prop_str_vals_t type.
+ */
+prop_str_vals_t get_string_properties (const table_entity::properties_type& properties) {
+  prop_str_vals_t values {};
+  for (const auto v : properties) {
+    if (v.second.property_type() == edm_type::string) {
+      values.push_back(make_pair(v.first,v.second.string_value()));
+    }
+    else {
+      // Force the value as string in any case
+      values.push_back(make_pair(v.first, v.second.str()));
+    }
+  }
+  return values;
+}
+
+/*
+  Return true if an HTTP request has a JSON body
+
+  This routine can be called multiple times on the same message.
+ */
+bool has_json_body (http_request message) {
+  return message.headers()["Content-type"] == "application/json";
+}
+
+/*
+  Given an HTTP message with a JSON body, return the JSON
+  body as an unordered map of strings to strings.
+  get_json_body and get_json_bourne are valid and identical function calls.
+
+  If the message has no JSON body, return an empty map.
+
+  THIS ROUTINE CAN ONLY BE CALLED ONCE FOR A GIVEN MESSAGE
+  (see http://microsoft.github.io/cpprestsdk/classweb_1_1http_1_1http__request.html#ae6c3d7532fe943de75dcc0445456cbc7
+  for source of this limit).
+
+  Note that all types of JSON values are returned as strings.
+  Use C++ conversion utilities to convert to numbers or dates
+  as necessary.
+ */
+unordered_map<string,string> get_json_body(http_request message) {
+  unordered_map<string,string> results {};
+  const http_headers& headers {message.headers()};
+  auto content_type (headers.find("Content-Type"));
+  if (content_type == headers.end() ||
+      content_type->second != "application/json")
+    return results;
+
+  value json{};
+  message.extract_json(true)
+    .then([&json](value v) -> bool
+          {
+            json = v;
+            return true;
+          })
+    .wait();
+
+  if (json.is_object()) {
+    for (const auto& v : json.as_object()) {
+      if (v.second.is_string()) {
+        results[v.first] = v.second.as_string();
+      }
+      else {
+        results[v.first] = v.second.serialize();
+      }
+    }
+  }
+  return results;
+}
+
+unordered_map<string,string> get_json_bourne(http_request message) {
+ return get_json_body(message);
+}
+
+/*
+  Return a token for 24 hours of access to the specified table,
+  for the single entity defind by the partition and row.
+
+  permissions: A bitwise OR ('|')  of table_shared_access_poligy::permission
+    constants.
+
+    For read-only:
+      table_shared_access_policy::permissions::read
+    For read and update:
+      table_shared_access_policy::permissions::read |
+      table_shared_access_policy::permissions::update
+ */
+pair<status_code,string> do_get_token (const cloud_table& data_table,
+                   const string& partition,
+                   const string& row,
+                   uint8_t permissions) {
+
+  utility::datetime exptime {utility::datetime::utc_now() + utility::datetime::from_days(1)};
+  try {
+    string limited_access_token {
+      data_table.get_shared_access_signature(table_shared_access_policy {
+                                               exptime,
+                                               permissions},
+                                             string(), // Unnamed policy
+                                             // Start of range (inclusive)
+                                             partition,
+                                             row,
+                                             // End of range (inclusive)
+                                             partition,
+                                             row)
+        // Following token allows read access to entire table
+        //table.get_shared_access_signature(table_shared_access_policy {exptime, permissions})
+      };
+    cout << "Token " << limited_access_token << endl;
+    return make_pair(status_codes::OK, limited_access_token);
+  }
+  catch (const storage_exception& e) {
+    cout << "Azure Table Storage error: " << e.what() << endl;
+    cout << e.result().extended_error().message() << endl;
+    return make_pair(status_codes::InternalError, string{});
+  }
+}
+
+//---------------------------------------------------------------------------------------
 
 void handle_post (http_request message) {
   //TODO Not implemented yet!
   message.reply(status_codes::NotImplemented);
+  
+  string path {uri::decode(message.relative_uri().path())};
+  cout << endl << "**** POST " << path << endl;
+  auto paths = uri::split_path(path);
+
+  // need operation name, country, username, status = 4
+  if (paths.size() < 4) {
+    message.reply(status_codes::BadRequest);
+    return;
+  }
+  else if(paths[0] != push_status_op) {
+    message.reply(status_codes::BadRequest);
+    return;
+  }
+  else if(!has_json_body(message)) {
+    message.reply(status_codes::BadRequest);
+    return;
+  }
+
+  unordered_map<string, string> json_body {get_json_bourne(message)};
+  unordered_map<string, string>::const_iterator json_body_password_iterator
+    {json_body.find("Friends")};
+  
+  if(json_body.size() != 1) {
+    message.reply(status_codes::BadRequest);
+    return;
+  }
+  // No 'Friends' property
+  else if(json_body_password_iterator == json_body.end()) {
+    message.reply(status_codes::BadRequest);
+    return;
+  }
+
+  
   return;
 }
 
 int main (int argc, char const * argv[]) {
   cout << "Parsing connection string" << endl;
-  table_cache.init (storage_connection_string);
+  //table_cache.init (storage_connection_string);
 
   cout << "Opening listener" << endl;
   http_listener listener {def_url};
